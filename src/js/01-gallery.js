0890692628691798e57e5ea68f326ab70af8f395
AntonPavlov8 <barsikstd@gmail.com>
2023-03-13 19:34:57 -0500

    task 1 and 2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+galleryItems.forEach(element => {
+  const galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery__item');
+  galleryItem.innerHTML = `<a class="gallery__link" href=${element.original} >
+    <img
+      class="gallery__image"
+      src=${element.preview}
+      data-source=${element.original}
+      alt=${element.description}
+    />
+  </a>`;
+  gallery.append(galleryItem);
+});
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import * as _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('time'));
+
+function gg(data) {
+  localStorage.setItem('time', data.seconds);
+  console.log(data);
+}
+
+player.on('timeupdate', _.throttle(gg, 1000));
