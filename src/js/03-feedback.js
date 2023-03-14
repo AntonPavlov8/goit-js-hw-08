944fb14d3c73a2b26c1a84d97ccaa555f9b1b290
AntonPavlov8 <barsikstd@gmail.com>
2023-03-13 21:38:12 -0500

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+
+if (localStorage.getItem('feedback-form-state')) {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  form.elements.email.value = data.email;
+  form.elements.message.value = data.message;
+}
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    const data = {
+      email: form.elements.email.value,
+      message: form.elements.message.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }, 500)
+);
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
