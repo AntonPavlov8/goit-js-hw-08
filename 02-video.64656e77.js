658e9967542305a67ab76d087ae8bd9d30daa6bc
AntonPavlov8 <112436831+AntonPavlov8@users.noreply.github.com>
2023-03-14 01:57:44 +0000

    Deploying to gh-pages from @ AntonPavlov8/goit-js-hw-08@82b67c029c49ccf8e0cfd3a8411663fa87915f3e 🚀

--- 02-video.64656e77.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.setCurrentTime(localStorage.getItem("time")),t.on("timeupdate",_.throttle((function(e){localStorage.setItem("time",e.seconds),console.log(e)}),1e3))}();
-//# sourceMappingURL=02-video.64656e77.js.map
